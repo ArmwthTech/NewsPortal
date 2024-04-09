ffea5ce90a58e3500d0afda3d9ac2814b37c8607
ArmWthTech <98inarik@gmail.com>
2024-04-09 20:54:54 +0500

    Home_ Pages
    add home pages

--- NewsPortalSF/main/views.py
+++ NewsPortalSF/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
+def index(request):
+    data = {
+        'title': 'Главная страница!!!',
+    }
+    return render(request, 'main/index.html',data)
+
+def about(request):
+    return render(request, 'main/about.html')
 # Create your views here.
